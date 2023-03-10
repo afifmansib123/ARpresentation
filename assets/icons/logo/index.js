e95401ba15c871b676a18e02ae248610cd3f6b63
afifmansib <afifmansib123@gmail.com>
2023-03-11 01:05:40 +0700

    jak

--- assets/icons/logo/index.js
+++ assets/icons/logo/index.js
@@ -1,6 +1,6 @@
 const Logo = () => {
     return (
-      <img src="/images/image11.jpg" alt="logo" width="100" height="70"/>
+      <img src="/images/logo.jpg" alt="logo" width="100" height="70"/>
     )
   };
   
